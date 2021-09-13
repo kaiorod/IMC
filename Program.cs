f75487c4b1cd20020ac936c3de8dd48d48a51bf5
kaiorod <caiorodrigo2775@gmail.com>
2021-09-13 00:10:19 -0300

    Finaliza IMC

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double alt, peso, imc;
+
+            Console.Beep();
+            Console.Clear();
+            Console.Write("Digite sua altura (metros) : ");
+            alt = double.Parse(Console.ReadLine());
+            Console.Write("Digite seu peso (kg) : ");
+            peso = double.Parse(Console.ReadLine());
+
+            imc = peso / Math.Pow(alt,2);
+
+            Console.WriteLine($"Altura : {alt:N2} kg \nPeso : {peso} m");
+            Console.Write($"\nIMC : ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine($"{imc:N2} kg/m^2");
+            Console.ResetColor();
+
+        }
+    }
+}
